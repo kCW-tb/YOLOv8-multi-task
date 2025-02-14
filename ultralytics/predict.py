e830786c9e9c804ede1270e043c1dce0200c2934
ChanWoo Kim <71691159+kCW-tb@users.noreply.github.com>
2025-02-14 14:19:04 +0900

    Update predict.py

--- ultralytics/predict.py
+++ ultralytics/predict.py
@@ -1,9 +1,11 @@
 import sys
-sys.path.insert(0, "/home/jiayuan/ultralytics-main/ultralytics")
+import torch
+
+sys.path.insert(0, "C:/YOLOv8-multi-task/ultralytics")
 
 from ultralytics import YOLO
 
-
 number = 3 #input how many tasks in your work
-model = YOLO('/home/jiayuan/ultralytics-main/ultralytics/runs/best.pt')  # Validate the model
-model.predict(source='/data/jiayuan/dash_camara_dataset/daytime', imgsz=(384,672), device=[3],name='v4_daytime', save=True, conf=0.25, iou=0.45, show_labels=False)
+model = YOLO('C:/YOLOv8-multi-task/runs/multi/yolopm/weights/best.pt')  # Validate the model
+#image size only -> width : 1280, height : 720
+model.predict(source='./drive', imgsz=(384,672), device=0,name='drive_predict', save=True, conf=0.25, iou=0.45, show_labels=False, speed=True)
